5e4bd88f2826eeafbff6c28d75fd7e3d8e71c6a4
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-05-23 22:39:51 +0300

    draft 0.2

--- services/email.py
+++ services/email.py
@@ -23,7 +23,19 @@ conf = ConnectionConfig(
 
 
 async def send_email(email: EmailStr, username: str, host: str):
-    try:
+    """
+The send_email function sends an email to the user with a link to confirm their email address.
+    The function takes in three parameters:
+        -email: EmailStr, the user's email address.
+        -username: str, the username of the user who is registering for an account.  This will be used in a greeting message within the body of the email sent to them.
+        -host: str, this is where we are hosting our application (i.e., localhost).  This will be used as part of a URL that they can click on within their browser.
+
+:param email: EmailStr: Validate the email address
+:param username: str: Pass the username of the user to be used in the email template
+:param host: str: Send the host name to the email template
+:return: A coroutine object
+"""
+try:
         token_verification = auth_service.create_email_token({"sub": email})
         message = MessageSchema(
             subject="Confirm your email ",
