8b6a11e2ef8fbefe875aac71df23a3354f899ad7
Nataleia Orlovska <101810309+Nataleia08@users.noreply.github.com>
2023-05-28 16:33:58 +0300

    draft 0.7

--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -1,11 +1,18 @@
+from unittest.mock import MagicMock, patch
+from database.models import User, Contact
+from services.auth import auth_service
+import pytest
+
+
+
 
 @pytest.fixture()
 def token(client, user, session, monkeypatch):
     mock_send_email = MagicMock()
-    monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
+    monkeypatch.setattr("routes.auth.send_email", mock_send_email)
     client.post("/api/auth/signup", json=user)
     current_user: User = session.query(User).filter(User.email == user.get('email')).first()
-    current_user.confirmed = True
+    current_user.confirmed_email = True
     session.commit()
     response = client.post(
         "/api/auth/login",
@@ -19,13 +26,13 @@ def test_create_contact(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
         response = client.post(
-            "/api/contact",
-            json={"name": "test_tag"},
+            "/api/contacts",
+            json={"email": "test@gamil.com", "first_name": "Tester", "last_name": "Testerovich", "phone_number": "+380668889900", "day_birthday": "2000-05-28"},
             headers={"Authorization": f"Bearer {token}"}
         )
         assert response.status_code == 201, response.text
         data = response.json()
-        assert data["name"] == "test_tag"
+        assert data["email"] == "test@gamil.com"
         assert "id" in data
 
 
@@ -33,12 +40,12 @@ def test_get_contact(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
         response = client.get(
-            "/api/contact/1",
+            "/api/contacts/1",
             headers={"Authorization": f"Bearer {token}"}
         )
         assert response.status_code == 200, response.text
         data = response.json()
-        assert data["name"] == "test_tag"
+        assert data["email"] == "test@gamil.com"
         assert "id" in data
 
 
@@ -46,39 +53,39 @@ def test_get_contact_not_found(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
         response = client.get(
-            "/api/contact/2",
+            "/api/contacts/2",
             headers={"Authorization": f"Bearer {token}"}
         )
         assert response.status_code == 404, response.text
         data = response.json()
-        assert data["detail"] == "Tag not found"
+        assert data["detail"] == "Contact not found"
 
 
-def test_get_contact(client, token):
+def test_get_contacts(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
         response = client.get(
-            "/api/contact",
+            "/api/contacts",
             headers={"Authorization": f"Bearer {token}"}
         )
         assert response.status_code == 200, response.text
         data = response.json()
         assert isinstance(data, list)
-        assert data[0]["name"] == "test_tag"
+        assert data[0]["email"] == "test@gamil.com"
         assert "id" in data[0]
 
 
-def test_update_tag(client, token):
+def test_update_contact(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
         response = client.put(
-            "/api/contact/1",
-            json={"name": "new_test_tag"},
+            "/api/contacts/1",
+            json={"email": "test1@gamil.com", "first_name": "Bob", "last_name": "Smit", "phone_number": "+380668889000", "day_birthday": "2000-05-26"},
             headers={"Authorization": f"Bearer {token}"}
         )
         assert response.status_code == 200, response.text
         data = response.json()
-        assert data["name"] == "new_test_tag"
+        assert data["email"] == "test1@gamil.com"
         assert "id" in data
 
 
@@ -86,8 +93,8 @@ def test_update_contact_not_found(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
         response = client.put(
-            "/api/tags/2",
-            json={"name": "new_test_tag"},
+            "/api/contacts/2",
+            json={"email": "test2@gamil.com", "first_name": "Bob", "last_name": "Smit", "phone_number": "+380668889000", "day_birthday": "2000-05-26"},
             headers={"Authorization": f"Bearer {token}"}
         )
         assert response.status_code == 404, response.text
@@ -99,12 +106,12 @@ def test_delete_contact(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
         response = client.delete(
-            "/api/contact/1",
+            "/api/contacts/1",
             headers={"Authorization": f"Bearer {token}"}
         )
         assert response.status_code == 200, response.text
         data = response.json()
-        assert data["name"] == "new_test_tag"
+        assert data["email"] == "test2@gamil.com"
         assert "id" in data
 
 
@@ -112,7 +119,7 @@ def test_repeat_delete_contact(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
         response = client.delete(
-            "/api/contact/1",
+            "/api/contacts/1",
             headers={"Authorization": f"Bearer {token}"}
         )
         assert response.status_code == 404, response.text
@@ -120,67 +127,3 @@ def test_repeat_delete_contact(client, token):
         assert data["detail"] == "Tag not found"
 
 
-def test_create_user(client, user, monkeypatch):
-    mock_send_email = MagicMock()
-    monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
-    response = client.post(
-        "/api/auth/signup",
-        json=user,
-    )
-    assert response.status_code == 201, response.text
-    data = response.json()
-    assert data["user"]["email"] == user.get("email")
-    assert "id" in data["user"]
-
-
-def test_repeat_create_user(client, user):
-    response = client.post(
-        "/api/auth/signup",
-        json=user,
-    )
-    assert response.status_code == 409, response.text
-    data = response.json()
-    assert data["detail"] == "Account already exists"
-
-
-def test_login_user_not_confirmed(client, user):
-    response = client.post(
-        "/api/auth/login",
-        data={"username": user.get('email'), "password": user.get('password')},
-    )
-    assert response.status_code == 401, response.text
-    data = response.json()
-    assert data["detail"] == "Email not confirmed"
-
-
-def test_login_user(client, session, user):
-    current_user: User = session.query(User).filter(User.email == user.get('email')).first()
-    current_user.confirmed = True
-    session.commit()
-    response = client.post(
-        "/api/auth/login",
-        data={"username": user.get('email'), "password": user.get('password')},
-    )
-    assert response.status_code == 200, response.text
-    data = response.json()
-    assert data["token_type"] == "bearer"
-
-
-def test_login_wrong_password(client, user):
-    response = client.post(
-        "/api/auth/login",
-        data={"username": user.get('email'), "password": 'password'},
-    )
-    assert response.status_code == 401, response.text
-    data = response.json()
-    assert data["detail"] == "Invalid password"
-
-
-def test_login_wrong_email(client, user):
-    response = client.post(
-        "/api/auth/login",
-        data={"username": 'email', "password": user.get('password')},
-    )
-    assert response.status_code == 401, response.text
-    data = response.json()
-    assert data["detail"] == "Invalid email"
