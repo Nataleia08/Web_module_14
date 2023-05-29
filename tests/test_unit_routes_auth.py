e6d1e9897d92f7f8fddac45496dca75b1d58b955
Nataleia Orlovska <101810309+Nataleia08@users.noreply.github.com>
2023-05-29 22:20:53 +0300

    draft 1.0

--- tests/test_unit_routes_auth.py
+++ tests/test_unit_routes_auth.py
@@ -2,8 +2,9 @@ import unittest
 from unittest.mock import MagicMock
 
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, HTTPException, Depends, status, Security, BackgroundTasks, Request
+from fastapi import HTTPException, Depends, status, Security, BackgroundTasks, Request
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 
 from database.models import User, Contact
 from schemas import ContactModel, ContactResponse, UserAuthModel, UserDb, UserAuthResponse, TokenModel, RequestEmail
@@ -34,22 +35,49 @@ class TestRoutsAuth(unittest.IsolatedAsyncioTestCase):
             await signup(body=body, background_tasks=background_tasks, request=requset, db=self.session)
 
     async def test_login_failed(self):
-        pass
-
-    async def test_login(self):
-        new_user = User(id=2, username="tester", email="test47@gmail.com", password="1234567")
-        self.session.query().filter().first.return_value = new_user
         body_oauth = MagicMock(spec=OAuth2PasswordRequestForm)
-        result = await login(body = body_oauth , db=self.session)
-        self.assertTrue(hasattr(result, "access_token"))
-        self.assertTrue(hasattr(result, "refresh_token"))
-        self.assertEqual(result["token_type"], "bearer")
+        body_oauth.username = "tester"
+        body_oauth.password = "1234567"
+        new_user = User(username="tester", email="test9@gmail.com", password="1234567")
+        self.session.query().filter().first.return_value = new_user
+        with self.assertRaises(HTTPException) as cm:
+            await login(body=body_oauth, db=self.session)
+
+
+
+    async def test_login(self): # ?????????????????
+        pass
+        # body_oauth = MagicMock(spec=OAuth2PasswordRequestForm)
+        # body_oauth.username = "tester"
+        # body_oauth.password ="1234567"
+        # body_oauth.confirmed_email = True
+        # user_data = User(confirmed_email = True, password ="1234567")
+        # self.session.query().filter().first.return_value = user_data
+        # result = await login(body = body_oauth , db=self.session)
+        # self.assertIsNotNone(hasattr(result["access_token"]))
+        # self.assertIsNotNone(hasattr(result["refresh_token"]))
+        # self.assertEqual(result["token_type"], "bearer")
+
 
     async def test_confirmed_email(self):
         pass
+        # token = MagicMock(spec=jwt.encode)
+        # user_data = User(email="test9@gmail.com", confirmed_email=False)
+        # self.session.query().filter().first.return_value = user_data
+        # self.session.auth_service.get_email_from_token(token) = user_data.email
+        # await confirmed_email(token = token, db=self.session)
+        # self.assertTrue(user_data.confirmed_email)
 
     async def test_confirmed_email_failed(self):
-        pass
+        token = MagicMock(spec=jwt.encode)
+        payload = MagicMock(jwt.decode)
+        user_data = User(email="test9@gmail.com", confirmed_email=True)
+        self.session.query().filter().first.return_value = user_data
+        email = "test9@gmail.com"
+        self.session.query().filter().first.return_value = email
+        with self.assertRaises(HTTPException) as cm:
+            await confirmed_email(token = token, db=self.session)
+
 
     async def test_refresh_token(self):
         pass
@@ -58,10 +86,23 @@ class TestRoutsAuth(unittest.IsolatedAsyncioTestCase):
         pass
 
     async def test_request_email(self):
-        pass
+        body = RequestEmail(email="test9@gmail.com")
+        background_tasks = MagicMock(spec=BackgroundTasks)
+        requset = MagicMock(spec=Request)
+        new_user = User(email="test9@gmail.com")
+        self.session.query().filter().first.return_value = new_user
+        result = await request_email(body = body, background_tasks = background_tasks, request = requset, db=self.session)
+        self.assertEqual(result["message"], "Check your email for confirmation.")
+
 
     async def test_request_email_failed(self):
-        pass
+        body = RequestEmail(email="test9@gmail.com")
+        background_tasks = MagicMock(spec=BackgroundTasks)
+        requset = MagicMock(spec=Request)
+        new_user = User(email="test9@gmail.com", confirmed_email = True)
+        self.session.query().filter().first.return_value = new_user
+        result = await request_email(body=body, background_tasks=background_tasks, request=requset, db=self.session)
+        self.assertEqual(result["message"], "Your email is already confirmed")
 
 
 
